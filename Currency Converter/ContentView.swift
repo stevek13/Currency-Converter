fee4fd350830edc70571685c263a9e89ff9e6ed6
Steve Kite <skite1@hotmail.com>
2026-01-13 09:38:27 -0800

    Main screen setup

--- Currency Converter/ContentView.swift
+++ Currency Converter/ContentView.swift
@@ -1,21 +1,104 @@
 //
 //  ContentView.swift
-//  Currency Converter
+//  currency converter
 //
-//  Created by Steve on 1/13/26.
+//  Created by Steve on 1/11/26.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var showExchangeInfo = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            // Background Image
+            Image(.background)
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                // Prancing Pony Image View
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                
+                // Exchange Text
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundStyle(Color(.white))
+                
+                // Conversion Section
+                HStack {
+                    // Left conversion section
+                    VStack {
+                        // Currency
+                        HStack {
+                            // currency image
+                            Image(.silverpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 33, height: 33)
+                            // currency text field
+                            Text("Silver Piece")
+                                .font(.headline)
+                                .foregroundStyle(Color(.white))
+                            
+                        }
+                        
+                        // Text Field
+                        Text("Text field")
+                            .foregroundStyle(.white)
+                    }
+                    
+                    // Equal Sign
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundStyle(Color(.white))
+                        .symbolEffect(.pulse )
+                    
+                    // Right conversion section
+                    VStack {
+                        HStack {
+                            // currency text field
+                            Text("Gold Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                            
+                            // currency image
+                            Image(.goldpiece)
+                                .resizable( )
+                                .scaledToFit( )
+                                .frame(width: 33, height: 33)
+                            
+                        }
+                        //Text Field
+                        Text("Text Field")
+                            .foregroundStyle(.white)
+                    }
+                }
+                
+                Spacer()
+                
+                // Info Button
+                HStack {
+                    
+                    Spacer()
+                    Button {
+                        showExchangeInfo.toggle()
+                        print("\(showExchangeInfo)")
+                    } label: {
+                        Image(systemName: "info.circle.fill")
+                            .font(Font.largeTitle.bold())
+                            .foregroundStyle(Color(.white))
+                    }
+                    .padding(.trailing)
+                }
+            }
+            .border(.blue)
         }
-        .padding()
     }
 }
 
