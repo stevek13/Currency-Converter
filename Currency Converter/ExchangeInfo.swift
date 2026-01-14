c4181d9605b893f4ec382c99d89df617b797cc4a
Steve Kite <skite1@hotmail.com>
2026-01-14 09:32:35 -0800

    Use custom views more than once

--- Currency Converter/ExchangeInfo.swift
+++ Currency Converter/ExchangeInfo.swift
@@ -9,7 +9,43 @@ import SwiftUI
 
 struct ExchangeInfo: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            //Background Parchment image
+            Image("parchment")
+                .resizable()
+                .ignoresSafeArea()
+                .background(.brown)
+            VStack{
+                // Title Text
+                Text("Exchange Rates")
+                    .font(.largeTitle)
+                    .tracking(3)
+                    .bold()
+                // Description Text
+                Text("Here at the Prancing Pony, we are happy to offer you a place where you can exchange all the known currencies in the entire world except one. We used to take Brandy Bucks, but after finding out that it was a person instead of a piece of paper, we realized it had no value to us. Below is a simple guide to our currency exchange rates:")
+                    .font(.title3)
+                    .padding(20)
+               
+                ExchangeRate(leftImage: .goldpiece, rightImage: .goldpenny, text: "1 Gold Piece = 4 Gold Pennies")
+                ExchangeRate(leftImage: .goldpenny, rightImage: .silverpiece, text: "1 Gold Penny = 4 Silver Pieces")
+                ExchangeRate(leftImage: .silverpiece, rightImage: .silverpenny, text: "1 Silver Piece = 4 Silver Pennies")
+                ExchangeRate(leftImage: .silverpenny, rightImage: .copperpenny, text: "1 Silver Penny = 100 Copper Pennies")
+                
+                // Done Button
+                Button("Done") {
+                    
+                }
+                .buttonStyle(BorderedProminentButtonStyle())
+                .tint(.brown.mix(with: .black, by: 0.2))
+                .font(.largeTitle)
+                .padding()
+                // Make Done button text color white
+                .foregroundStyle(Color(.white))
+                
+            }
+            // Make entire screen font black
+            .foregroundStyle(Color(.black ))
+        }
     }
 }
 
--- Currency Converter/ExchangeRate.swift
+++ Currency Converter/ExchangeRate.swift
@@ -8,11 +8,32 @@
 import SwiftUI
 
 struct ExchangeRate: View {
+    
+    let leftImage: ImageResource
+    let rightImage: ImageResource
+    let text: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // Exchange Rates
+        HStack{
+            // Left Currency Image
+            Image(leftImage)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 33)
+            
+            // Exchange Rate Text
+            Text(text)
+            
+            // Right Currency Image
+            Image(rightImage)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 33)
+            
+        }
     }
 }
-
 #Preview {
-    ExchangeRate()
+    ExchangeRate(leftImage: .silverpiece, rightImage: .silverpenny, text: "1 Silver Piece = 4 Silver Pennies")
 }
